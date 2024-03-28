baf1e105fa9c1bb116481236346d0e6c40f0d017
coleburch <coleburch14@gmail.com>
2024-03-27 18:24:05 -0600

    MVP of the extension.
    This extension grabs the post ID of an ad on Facebook and then sends an
    alert to the user with the ID so the user can copy it faster.

---
+++ grabber.js
@@ -0,0 +1,11 @@
+const post_script = document.querySelectorAll('script[type="application/json"]');
+
+for (let i = 0; i < post_script.length; i++) {
+    let script = post_script[i].innerHTML;
+    const regex = /subscription_target_id/;
+    if (regex.test(script)) {
+        const regex2 = /"subscription_target_id":"(\d+)"/;
+        const match = regex2.exec(script);
+        alert("post id is " + match[1]);
+    }
+}
