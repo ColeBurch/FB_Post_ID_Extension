1d2799aaa1a1d97b5def86bda1e6d5f6ad40147c
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2024-04-01 18:44:14 -0600

    Editing Manifest file for upload to Chrome Web Store.

--- grabber.js
+++ grabber.js
@@ -1,11 +1,13 @@
-const post_script = document.querySelectorAll('script[type="application/json"]');
+const post_script = document.querySelectorAll(
+  'script[type="application/json"]'
+);
 
 for (let i = 0; i < post_script.length; i++) {
-    let script = post_script[i].innerHTML;
-    const regex = /subscription_target_id/;
-    if (regex.test(script)) {
-        const regex2 = /"subscription_target_id":"(\d+)"/;
-        const match = regex2.exec(script);
-        alert("post id is " + match[1]);
-    }
+  let script = post_script[i].innerHTML;
+  const regex = /subscription_target_id/;
+  if (regex.test(script)) {
+    const regex2 = /"subscription_target_id":"(\d+)"/;
+    const match = regex2.exec(script);
+    alert("post id is " + match[1]);
+  }
 }
